4013234fc97d0a84f883e21104c6401458474852
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2017-05-14 12:50:33 +0300

    Revert

--- rssrelay.rb
+++ rssrelay.rb
@@ -62,19 +62,18 @@ class RSSRelay < Sinatra::Application
       # Retrieve and parse the feed from the URL provided
       open(params[:url]) do |rss|
         feed = RSS::Parser.parse(rss, false)
-        items = feed.items
-#        items = feed.items.map{|item| 
-#          {
-#          :title => item.title, 
-#          :link => item.link,
-#          :description => item.description,
-#          :published => 
-#            if feed.class.to_s == "RSS::RDF"
-#              item.dc_date
-#            else
-#              item.pubDate
-#            end
-#          }
+        items = feed.items.map{|item| 
+          {
+          :title => item.title, 
+          :link => item.link,
+          :description => item.description,
+          :published => 
+            if feed.class.to_s == "RSS::RDF"
+              item.dc_date
+            else
+              item.pubDate
+            end
+          }
         }
       end
 
